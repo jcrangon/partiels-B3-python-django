68c253220b604c01befc702d0121d8a781140be8
jcrangon <jc.rangon@gmail.com>
2025-01-17 13:27:03 +0100

    typo fix

--- converter/app/urls.py
+++ converter/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.landingpage, name="landingpage"),
-    path("login", views.login, name="loginpage"),
+    path("login", views.login_view, name="loginpage"),
     path("qcm", views.qcm, name="qcmpage"),
 ]
